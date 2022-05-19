08bd9104968c2cb06ddf55ffa1bb16544c9ef39e
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2022-05-19 10:21:24 +0500

    init

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var hello = alert("Assalomu Aleykum");
+var seconrdy = alert("Xush kelibsiz");
+var userName = prompt("Ismingizni kriting !", "Muhammadali");
+var sureName = prompt("Familiyangizni kriting !", "Shuhratjonov");
+var age = prompt("Yoshingizni kriting !", "19");
+var Study = prompt("Qayerda o'qiysiz ?", "Najot Ta'lim");
+var isMarried = prompt("Turmush qurganmisiz ?", "Ha");
+var doYouKnowComputerProgramm = ("Dasturlashni bilasizmi ?", "Ha");
+var leanguage = prompt("Qaysi tillarni bilasiz ?", "Uzbek");
+var aboutYou = prompt("O'zingiz haqingizda", "My dream is ....");
+
+console.log("Ismingiz: " + userName + "/n" + "Familiyangiz: " + sureName + "/n" + "Yoshingiz: " + age + "/n" + "Qayerda o'qiysiz: " + Study + "/n" + "Turmush qurganmisiz: " + isMarried + "/n" + "Dasturlashni bilasizmi: " + doYouKnowComputerProgramm + "/n" + "Qaysi tillarni bilasiz: " + leanguage + "/n" + "O'zingiz haqingizda: " + aboutYou);
